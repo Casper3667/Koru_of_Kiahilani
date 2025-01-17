10fef43057d89a615a3e1da1df01da7d65388b4a
TGW <CasperMC@hotmail.dk>
2025-01-17 22:38:49 +0100

    Add project files.

---
+++ Koru_Test/Test1.cs
@@ -0,0 +1,22 @@
+﻿using Koru_of_Kiahilani.Commands;
+
+namespace Koru_Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestTheCommand()
+        {
+            string testResult = TestCommand.CommandoTest();
+            string expectedResult = "Hello";
+            Assert.AreEqual(testResult, expectedResult);
+        }
+    }
+}
---
+++ Koru_of_Kiahilani/Commands/TestCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koru_of_Kiahilani.Commands
+{
+    public class TestCommand
+    {
+        public static string CommandoTest()
+        {
+            return "Hello";
+        }
+    }
+}
